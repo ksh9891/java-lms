28d6d23f8a7ab1db85922965f1d2b127f9af8ba0
SangHyun Kim <ksh9891@gmail.com>
2024-10-24 22:19:08 +0900

    feat(lms): Session 객체 생성 시 기간에 대한 값이 입력되지 않으면 예외가 발생하도록 추가
    - 기간, 모집기간에 대한 예외처리 추가
    - 테스트 케이스 추가
    - 테스트 문서 체크

--- src/main/java/nextstep/courses/domain/Session.java
+++ src/main/java/nextstep/courses/domain/Session.java
@@ -2,8 +2,18 @@ package nextstep.courses.domain;
 
 public class Session {
     private final DateRange sessionDateRange;
+    private final DateRange sessionRecruitmentDateRange;
 
-    public Session(final DateRange sessionDateRange) {
+    public Session(final DateRange sessionDateRange, final DateRange sessionRecruitmentDateRange) {
+        if (sessionDateRange == null) {
+            throw new IllegalArgumentException("세션 기간은 필수압니다.");
+        }
+
+        if (sessionRecruitmentDateRange == null) {
+            throw new IllegalArgumentException("세션 모집기간은 필수입니다.");
+        }
+
         this.sessionDateRange = sessionDateRange;
+        this.sessionRecruitmentDateRange = sessionRecruitmentDateRange;
     }
 }
--- src/test/java/nextstep/courses/domain/SessionTest.java
+++ src/test/java/nextstep/courses/domain/SessionTest.java
@@ -1,4 +1,27 @@
 package nextstep.courses.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
 public class SessionTest {
+    @Test
+    @DisplayName("Session 기간은 필수이다.")
+    void shouldThrowExceptionWhenSessionDateRangeIsMissing() {
+        final DateRange recruitmentDateRange = new DateRange(LocalDate.of(2024, 10, 1), LocalDate.of(2024, 10, 10));
+
+        assertThatIllegalArgumentException()
+            .isThrownBy(() -> new Session(null, recruitmentDateRange));
+    }
+
+    @Test
+    @DisplayName("Session 모집기간은 필수이다.")
+    void shouldThrowExceptionWhenSessionRecruitmentDateRangeIsMissing() {
+        final DateRange sessionDateRange = new DateRange(LocalDate.of(2024, 10, 1), LocalDate.of(2024, 10, 10));
+        assertThatIllegalArgumentException()
+            .isThrownBy(() -> new Session(sessionDateRange, null));
+    }
 }
