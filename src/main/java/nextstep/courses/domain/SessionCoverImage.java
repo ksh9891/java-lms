7ea5902edaf472bb75e808e26b04cdfffb725a3c
SangHyun Kim <ksh9891@gmail.com>
2024-10-27 22:42:07 +0900

    feat(lms): SessionCoverImage 객체 추가

---
+++ src/main/java/nextstep/courses/domain/SessionCoverImage.java
@@ -0,0 +1,15 @@
+package nextstep.courses.domain;
+
+public class SessionCoverImage {
+    private final String name;
+    private final ImageExtension extension;
+    private final ImageProperties properties;
+    private final ImageSize size;
+
+    public SessionCoverImage(final String name, final ImageExtension extension, final ImageProperties properties, final ImageSize size) {
+        this.name = name;
+        this.extension = extension;
+        this.properties = properties;
+        this.size = size;
+    }
+}
