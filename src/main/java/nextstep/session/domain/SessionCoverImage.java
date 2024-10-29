d9643f3970ebe498e79bdd963db4d9f146f0c29a
SangHyun Kim <ksh9891@gmail.com>
2024-10-29 22:16:36 +0900

    fix(lms): 필드명 수정

--- src/main/java/nextstep/session/domain/SessionCoverImage.java
+++ src/main/java/nextstep/session/domain/SessionCoverImage.java
@@ -3,13 +3,13 @@ package nextstep.session.domain;
 public class SessionCoverImage {
     private final String name;
     private final ImageExtension extension;
-    private final ImageDimensions properties;
+    private final ImageDimensions dimensions;
     private final ImageSize size;
 
-    public SessionCoverImage(final String name, final ImageExtension extension, final ImageDimensions properties, final ImageSize size) {
+    public SessionCoverImage(final String name, final ImageExtension extension, final ImageDimensions dimensions, final ImageSize size) {
         this.name = name;
         this.extension = extension;
-        this.properties = properties;
+        this.dimensions = dimensions;
         this.size = size;
     }
 }
