74c4fe152fb6d07deb7209c4954e164084d57db8
SangHyun Kim <ksh9891@gmail.com>
2024-11-01 22:50:09 +0900

    refactor(lms): 전달받은 이름으로 Enum 을 찾을 수 있는 정적 메서드 추가

--- src/main/java/nextstep/session/domain/SessionStatus.java
+++ src/main/java/nextstep/session/domain/SessionStatus.java
@@ -1,9 +1,18 @@
 package nextstep.session.domain;
 
+import java.util.Arrays;
+
 public enum SessionStatus {
     준비중, 모집중, 종료;
 
     public boolean isRecruit() {
         return this == 모집중;
+    }
+
+    public static SessionStatus fromName(final String name) {
+        return Arrays.stream(values())
+            .filter(value -> value.name().equals(name))
+            .findFirst()
+            .orElseThrow(() -> new IllegalArgumentException("상태를 찾을 수 없습니다."));
     }
 }
