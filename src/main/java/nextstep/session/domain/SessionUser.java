eadd4cc23029e204ad65b96b4d0ec72578d9c33e
SangHyun Kim <ksh9891@gmail.com>
2024-11-02 00:21:39 +0900

    refactor(lms): SessionUser createdAt, updatedAt 추가

--- src/main/java/nextstep/session/domain/SessionUser.java
+++ src/main/java/nextstep/session/domain/SessionUser.java
@@ -2,13 +2,19 @@ package nextstep.session.domain;
 
 import nextstep.users.domain.NsUser;
 
+import java.time.LocalDateTime;
+
 public class SessionUser {
     private final NsUser user;
     private final Session session;
+    private final LocalDateTime createdAt;
+    private final LocalDateTime updatedAt;
 
     public SessionUser(final NsUser user, final Session session) {
         this.user = user;
         this.session = session;
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = null;
     }
 
     public boolean matchSessionUser(final SessionUser sessionUser) {
