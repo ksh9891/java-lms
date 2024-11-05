324d3fd851aa45651a6c26b67c641c17e8481f50
SangHyun Kim <ksh9891@gmail.com>
2024-11-05 12:02:10 +0900

    refactor(lms): Session equals, hashcode 구현 및 matchSession 수정

--- src/main/java/nextstep/session/domain/Session.java
+++ src/main/java/nextstep/session/domain/Session.java
@@ -5,6 +5,7 @@ import nextstep.users.domain.NsUser;
 
 import java.math.BigInteger;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 public class Session {
     private Long id;
@@ -134,7 +135,7 @@ public class Session {
     }
 
     public boolean matchSession(final Session target) {
-        return id.equals(target.id);
+        return this.equals(target);
     }
 
     public boolean hasLimit() {
@@ -183,5 +184,18 @@ public class Session {
 
     public SessionCoverImage getSessionCoverImage() {
         return sessionCoverImage;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final Session session = (Session) o;
+        return Objects.equals(getId(), session.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
     }
 }
